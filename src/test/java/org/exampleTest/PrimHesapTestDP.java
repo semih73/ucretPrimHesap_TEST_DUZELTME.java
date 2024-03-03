ae7eaf691c8e20e6b2e0a12e7e2512199b0c85f1
semih73 <semih.dinler@gmail.com>
2024-03-03 23:04:37 +0300

    Ders18 ODEV5 testi 2 bölüm testng xml dosyası ile_SON Eklemeler2

--- src/test/java/org/exampleTest/PrimHesapTestDP.java
+++ src/test/java/org/exampleTest/PrimHesapTestDP.java
@@ -8,27 +8,27 @@ import static org.testng.Assert.assertEquals;
 public class PrimHesapTestDP {
 
     @Test(groups = "toplamgelir", dataProvider = "primHakki")
-    public void primli(int sayi, int gunlukCalismaUcreti, int primGunlukUcret) {
+    public void primli(int sayi, int gunlukCalismaUcreti, int primGunlukUcret, int expectedUcret) {
         int testResultDP = UcretFormul.ucretHesapla(sayi);
-        assertEquals(testResultDP, 16300);
+        assertEquals(testResultDP, expectedUcret);
     }
 
     @DataProvider(name = "primHakki")
     public Object[][] primHakki() {
         return new Object[][]{
-                {26, 600, 700}
+                {26, 600, 700, 16300}
         };
     }
     @Test(groups = "toplamgelir", dataProvider = "primHakki2")
-    public void primsiz(int sayi, int gunlukCalismaUcreti, int primGunlukUcret) {
+    public void primsiz(int sayi, int gunlukCalismaUcreti, int primGunlukUcret, int expectedUcret2) {
 
         int testResultDP = UcretFormul.ucretHesapla(sayi);
-        assertEquals(testResultDP, 14400);
+        assertEquals(testResultDP, expectedUcret2);
     }
     @DataProvider(name = "primHakki2")
     public Object[][] primHakki2() {
         return new Object[][]{
-                {24, 600, 700}
+                {24, 600, 700, 14400}
         };
     }
 }
